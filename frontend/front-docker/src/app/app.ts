5871f872c24e81f1d3fa7344274cd111ba3146a0
Andrea Esplugas <aesplugas@insgabrielamistral.cat>
2026-02-10 20:24:12 +0100

    Second commit

--- frontend/front-docker/src/app/app.ts
+++ frontend/front-docker/src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-docker');
+  nom = '';
+  resposta = '';
+
+  constructor(private http: HttpClient) {}
+
+  enviarNom() {
+    this.http.post<any>('http://localhost:3000/api/hello', {
+      nom: this.nom
+    }).subscribe({
+      next: res => this.resposta = res.missatge,
+      error: () => this.resposta = 'Error en el servidor'
+    });
+  }
 }
