ea00f31fa8cbdc0ab15ac1fcd8ba00b4cf6c9b41
ycazalla1 <ycazaa1@gmail.com>
2026-02-13 19:49:40 +0100

    Final Commit

--- frontend/front-docker/src/app/app.ts
+++ frontend/front-docker/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, FormsModule],
+  standalone: true,
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,14 +16,27 @@ export class App {
   nom = '';
   resposta = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {
+  }
 
   enviarNom() {
+    console.log("Nom enviat:", this.nom);
     this.http.post<any>('http://localhost:3000/api/hello', {
       nom: this.nom
     }).subscribe({
-      next: res => this.resposta = res.missatge,
-      error: () => this.resposta = 'Error en el servidor'
+      // next: res => this.resposta = res.missatge,
+      // error: () => this.resposta = 'Error en el servidor'
+      next: res => {
+        this.resposta = res.missatge;
+        this.cdr.detectChanges();
+      },
+      error: () => {
+        this.resposta = 'Error en el servidor';
+        this.cdr.detectChanges();
+      }
     });
   }
 }
